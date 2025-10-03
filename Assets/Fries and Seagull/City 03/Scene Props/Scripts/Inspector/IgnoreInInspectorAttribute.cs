00e4eebc9ee48d4c5b3432f276153369eab7abc1
zohaibJR <zohaibrehman595@gmail.com>
2025-10-03 11:04:36 +0500

    Set Parking Curve
    Set Parking Curve

--- Assets/Fries and Seagull/City 03/Scene Props/Scripts/Inspector/IgnoreInInspectorAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Seagull.City_03.Inspector {
-    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-    public class IgnoreInInspectorAttribute : Attribute {
-        
-    }
-}
